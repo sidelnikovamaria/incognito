a056d20a2a82690781e0ecb8fa19586371e09296
sidelnikovamaria <145535838+sidelnikovamaria@users.noreply.github.com>
2023-11-29 00:45:17 +0300

    Update pr7(10).py

--- pr7/pr7(10).py
+++ pr7/pr7(10).py
@@ -4,7 +4,7 @@ b = input()
 c = input()
 x = 0
 for i in range(100, n + 1):
-    digit = str(i)
-    if a in digit and  b  in  digit and c in digit:
+    z = str(i)
+    if a in z and  b  in  z and c in z:
         x += 1
 print(x)
