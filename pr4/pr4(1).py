3ba4f3cc0e178b2989419141c3d127b59ea54f8e
sidelnikovamaria <145535838+sidelnikovamaria@users.noreply.github.com>
2023-11-29 00:26:41 +0300

    pr4(1).py

--- pr4/pr4(1).py
+++ pr4/pr4(1).py
@@ -1,6 +1,6 @@
 m = int(input(''))
 k = int(input(''))
-feb_symm = 0
+symm = 0
 i = 1
 a = 0
 b = 1
@@ -8,6 +8,6 @@ for i in range(m + 1):
     c = a + b
     a, b = b, c
     if (i >= k) and (i <= (m+1)):
-        feb_symm += c
+        symm += c
     i += 1
-print(feb_symm)
+print(symm)
