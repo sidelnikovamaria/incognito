e20096d3446730a04ae49509348e0e51cabea7d0
sidelnikovamaria <145535838+sidelnikovamaria@users.noreply.github.com>
2023-11-28 23:33:20 +0300

    pr8(1).py

--- pr8/pr8(1).py
+++ pr8/pr8(1).py
@@ -1,14 +1,20 @@
-import random
-pol = 0
-s = 0
 n = int(input("Ввод: "))
-a = [[random.randrange(10) for i in range(n)] for j in range(n)]
+a = []
+num = 0
+s = 0
 for i in range(n):
-    for j in range(i + 1, n):
-        if a[i][j] <= 0:
-            continue
+    x = []
+    for j in range(n):
+        x.append(int(input()))
+    a.append(x)
+for i in a:
+    print()
+    for j in i:
+        print(j, end = '')
+for i in range(n):
+    for j in range(i + 1, n)
         if a[i][j] > 0:
-            pol += 1
+            num += 1
             s += a[i][j]
 print("Сумма:", s)
-print("Число:", pol)
+print("Число:", num)
