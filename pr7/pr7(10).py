e795afc169fdd1189b7f94456d8b84e48a18e4d4
sidelnikovamaria <145535838+sidelnikovamaria@users.noreply.github.com>
2023-12-06 14:38:50 +0300

    Update pr7(10).py

--- pr7/pr7(10).py
+++ pr7/pr7(10).py
@@ -4,10 +4,11 @@ a = input()
 b = input()
 c = input()
 x = 0
-for i in range(100, n + 1):
-    z = str(i)
-    if a in z and  b  in  z and c in z:
-        x += 1
+if 210 < n < 231:
+    for i in range(100, n + 1):
+        z = str(i)
+        if a in z and  b  in  z and c in z:
+            x += 1
 print(x)
 
 print('Задание №2')
