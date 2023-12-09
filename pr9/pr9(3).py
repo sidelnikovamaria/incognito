ae49759bc80df913c058ce514e82d470cccfb60e
sidelnikovamaria <145535838+sidelnikovamaria@users.noreply.github.com>
2023-12-09 04:02:29 +0300

    Update pr9(3).py

--- pr9/pr9(3).py
+++ pr9/pr9(3).py
@@ -1,13 +1,9 @@
-n = int(input())
-def k(n):
-    if 0 < n < 10:
-        resulat = 'Простое'
-    else:
-        for i in range(2, n):
-            if n % 1 == 0:
-                resulat = 'Сложное'
-                break
-            else:
-                resulat = 'Простое'
-        return(resulat)
-print(k(n))
+def f():
+    b=int(input()) 
+    for i in range(b):
+        a=int(input()) 
+        if i%2==1:
+            print(a)
+        if a==0:
+            break
+f()
